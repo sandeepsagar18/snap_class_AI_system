782f5204de9e69c3cbbcf7df9e2533a7bd012ead
Hamza Rizvi <hamza@brew.tv>
2026-04-01 09:36:52 +0530

    Finishing Touches

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -49,9 +49,9 @@ def style_base_layout():
                 
          /* Hide Top Bar of streamlit */
                 
-            # #MainMenu, footer, header {
-            #     visibility: hidden;
-            # }
+            #MainMenu, footer, header {
+                visibility: hidden;
+            }
                 
             .block-container {
                 padding-top:1.5rem !important;    
